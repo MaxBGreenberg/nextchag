6ee9c9d36b7e19b5c5a21f6050448cb1dc9eee8b
MaxBGreenberg <maxbgreenberg@gmail.com>
2024-01-23 18:19:44 -0500

    added missing line of code so script actually for real functions now added notes to explain what script is doing

--- nextchag.sh
+++ nextchag.sh
@@ -1,14 +1,14 @@
-# Shell script to print the next Yom Tov 
+# Shell script to print the next Yom Tov
 #!/bin/sh
 
-TODAY=$(date -d 'today' +%s)
-NEXTYEAR=$(date -d "next year" +%Y)
-while IFS= read -r line; do
-        DATE=$(echo "$line" | awk '{print $1}')
-        DATE_UNIX=$(date -d "$DATE" +%s)
-        CHAG=$(echo "$line" | sed 's/^[^ ]* //')
-        if [[ $DATE_UNIX -ge $TODAY ]]; then
-                echo "The next chag is $CHAG."
+TODAY=$(date -d 'today' +%s)						# Today's date as UNIX timestamp at midnight
+NEXTYEAR=$(date -d "next year" +%Y)					# Next year in full
+while IFS= read -r line; do						# Loop through list
+        DATE=$(echo "$line" | awk '{print $1}')				# Get first word of each line
+        DATE_UNIX=$(date -d "$DATE" +%s)				# Convert first of each line to UNIX timestamp
+        CHAG=$(echo "$line" | sed 's/^[^ ]* //')			# Get rest of line after first white space
+        if [[ $DATE_UNIX -ge $TODAY ]]; then				# Check if unix timestamp for chag is greater than unix timestamp for today
+                echo "The next chag is $CHAG."				# Echo next chag to standard output
                 exit 0
         fi
-done
+done < <(hebcal --chag-only && hebcal --chag-only $NEXTYEAR)		# Feed list of chagim for this year and next year into while loop

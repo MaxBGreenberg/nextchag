722120640015f0e99cd4f7f7716d7ddaa8b133ef
MaxBGreenberg <maxbgreenberg@gmail.com>
2024-02-22 19:19:38 -0500

    Filter out Erev Chag

--- nextchag.sh
+++ nextchag.sh
@@ -8,6 +8,9 @@ while IFS= read -r line; do						# Loop through list
         DATE=$(echo "$line" | awk '{print $1}')				# Get first word of each line
         DATE_UNIX=$(date -d "$DATE" +%s)				# Convert first of each line to UNIX timestamp
         CHAG=$(echo "$line" | sed 's/^[^ ]* //')			# Get rest of line after first white space
+	if [[ "$line" == *Erev* || "$line" == *C''M* || "$line" == *Hoshana Raba* ]]; then
+		continue
+	fi
         if [[ $DATE_UNIX -ge $TODAY ]]; then				# Check if unix timestamp for chag is greater than unix timestamp for today
                 echo "The next chag is $CHAG."				# Echo next chag to standard output
                 exit 0
